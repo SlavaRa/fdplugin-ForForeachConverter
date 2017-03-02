282d27eff1cd513fec8318be02504c0ac6cbc958
Slava Buynov <SlavaB@playtika.com>
2017-03-02 19:57:00 +0300

    Init

---
+++ ForForeachConverter/PluginMain.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using PluginCore;
+using PluginCore.Helpers;
+using PluginCore.Utilities;
+
+namespace ForForeachConverter
+{
+    public class PluginMain : IPlugin
+    {
+        string settingFilename;
+
+        #region Required Properties
+
+        public int Api => 1;
+        public string Name => nameof(ForForeachConverter);
+        public string Guid => "21d9ab3e-93e4-4460-9298-c62f87eed7ba";
+        public string Help => string.Empty;
+        public string Author => "SlavaRa";
+        public string Description => string.Empty;
+        public object Settings { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Initializes the plugin
+        /// </summary>
+        public void Initialize()
+        {
+            InitBasics();
+            LoadSettings();
+            AddEventHandlers();
+        }
+
+        /// <summary>
+        /// Disposes the plugin
+        /// </summary>
+        public void Dispose() => SaveSettings();
+
+        /// <summary>
+        /// Handles the incoming events
+        /// </summary>
+        public void HandleEvent(object sender, NotifyEvent e, HandlingPriority priority)
+        {
+        }
+
+        /// <summary>
+        /// Initializes important variables
+        /// </summary>
+        void InitBasics()
+        {
+            var path = Path.Combine(PathHelper.DataDir, Name);
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            settingFilename = Path.Combine(path, "Settings.fdb");
+        }
+
+        /// <summary>
+        /// Loads the plugin settings
+        /// </summary>
+        void LoadSettings()
+        {
+            Settings = new Settings();
+            if (!File.Exists(settingFilename)) SaveSettings();
+            else Settings = (Settings) ObjectSerializer.Deserialize(settingFilename, Settings);
+        }
+
+        /// <summary>
+        /// Adds the required event handlers
+        /// </summary>
+        void AddEventHandlers()
+        {
+        }
+
+        /// <summary>
+        /// Saves the plugin settings
+        /// </summary>
+        void SaveSettings() => ObjectSerializer.Serialize(settingFilename, Settings);
+    }
+}
---
+++ ForForeachConverter/Settings.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ForForeachConverter
+{
+    [Serializable]
+    internal class Settings
+    {
+    }
+}
