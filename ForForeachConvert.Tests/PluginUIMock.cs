fcee05fec089b3620affceea43b647b1fef6767d
Slava Buynov <SlavaB@playtika.com>
2017-03-02 20:41:59 +0300

    Add ForForeachCoverter.Tests

---
+++ ForForeachConvert.Tests/PluginUIMock.cs
@@ -0,0 +1,11 @@
+﻿using ASCompletion;
+
+namespace ForForeachConverter.Tests
+{
+    internal class PluginUIMock : PluginUI
+    {
+        public PluginUIMock(ASCompletion.PluginMain plugin) : base(plugin)
+        {
+        }
+    }
+}
